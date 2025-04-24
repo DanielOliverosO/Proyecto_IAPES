5d1e1fb619af3095288c2d9e39021085b7bc5978
Daniel <162449766+legosmixels@users.noreply.github.com>
2025-04-24 00:39:02 -0500

    Add files via upload

---
+++ IAPES/src/main/java/com/proyecto/IAPES/IapesApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.IAPES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IapesApplication.class, args);
+	}
+
+}
