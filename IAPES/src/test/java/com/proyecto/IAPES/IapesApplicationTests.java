8301ba01549eb3f8a199c5d731c4df3ff5feb3b9
Daniel <legosmixels@gmail.com>
2025-04-26 10:36:49 -0500

    Agregando Proyecto_IAPES

---
+++ IAPES/src/test/java/com/proyecto/IAPES/IapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.IAPES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
